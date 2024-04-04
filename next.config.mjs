a5cb46d8d3d5635a7a54492c6ea0c9608f167bd6
dineshba25 <dineshba63@gmail.com>
2024-04-04 12:56:09 -0500

    Added unsorted library of app pictures

---
+++ next.config.mjs
@@ -0,0 +1,10 @@
+// next.config.mjs
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        domains: ['images.unsplash.com'],
+    },
+    // other configurations...
+};
+
+export default nextConfig;
