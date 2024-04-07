879558bbe511453f2fc6f0e98f0f51cb52cefcda
dineshba25 <dineshba63@gmail.com>
2024-04-06 23:50:41 -0500

    Made scroll prompt less annoying (no text, minimized movement and time on screen)

--- app/ScrollDownPrompt.tsx
+++ app/ScrollDownPrompt.tsx
@@ -4,7 +4,18 @@ import { motion } from 'framer-motion';
 export function ScrollDownPrompt() {
     const [isVisible, setIsVisible] = useState(true);
 
+    useEffect(() => {
+        const handleScroll = () => {
+            setIsVisible(window.scrollY === 0);
+        };
 
+        window.addEventListener('scroll', handleScroll);
+
+        // Clean up the event listener when the component unmounts
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []); // Empty dependency array means this effect runs once on mount and clean up on unmount
 
     const arrowAnimation = {
         initial: { y: -10, opacity: 0 },
@@ -12,17 +23,15 @@ export function ScrollDownPrompt() {
     };
 
     return (
-        <div>
+        <div style={{ position: 'fixed', zIndex: 1000, bottom: 50, left: "50%", transform: "translateX(-50%)" }}>
             {isVisible && (
-                    <motion.div {...arrowAnimation}>
-                        <center>
-                            <div className={"text-gray-700"}>Scroll down to learn more</div>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" className="bi bi-arrow-down text-gray-500" viewBox="0 0 16 16">
-                                <path fillRule="evenodd" d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 1 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z"/>
-                            </svg>
-                        </center>
-
-                    </motion.div>
+                <motion.div {...arrowAnimation}>
+                    <center>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="white" className="bi bi-arrow-down text-gray-500" viewBox="0 0 16 16">
+                            <path fillRule="evenodd" d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 1 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z"/>
+                        </svg>
+                    </center>
+                </motion.div>
             )}
         </div>
     );
