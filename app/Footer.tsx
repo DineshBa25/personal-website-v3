b5d8c9f077ed1a0def67c97b2abdff771e45778e
Dinesh Balakrishnan <dineshba63@gmail.com>
2024-07-14 00:30:10 -0500

    added footer with dynamic last updated date based on commits

--- app/Footer.tsx
+++ app/Footer.tsx
@@ -1,7 +1,22 @@
+import React, { useEffect, useState } from 'react';
+
 export function Footer() {
+    const [lastUpdated, setLastUpdated] = useState(null);
+
+    useEffect(() => {
+        fetch('https://api.github.com/repos/DineshBa25/personal-website-v3/commits')
+            .then(response => response.json())
+            .then(data => {
+                const latestCommitDate = data[0].commit.author.date;
+                // @ts-ignore
+                setLastUpdated(new Date(latestCommitDate).toLocaleDateString());
+            });
+    }, []);
+
     return (
-        <footer className="w-full text-center border-t border-gray-900 p-4 pin-b text-gray-700">
+        <footer className="w-full text-center p-4 pin-b text-gray-700">
             Custom designed & built by Dinesh
+            {lastUpdated && <p>Last updated: {lastUpdated}</p>}
         </footer>
     );
 }
